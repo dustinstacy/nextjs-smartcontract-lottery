3e0de485193fdea334bff18be6e9ab71edac8156
Dustin Stacy <dustin.stacy@outlook.com>
2023-08-26 17:54:02 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@components/Header'
+
+const Home = () => {
+    return (
+        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+            <Header />
+        </main>
+    )
+}
+
+export default Home
